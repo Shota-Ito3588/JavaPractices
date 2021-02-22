70286651ac8fa2b6ff60119b752fec0cec3e1ca8
Shota <s.ito@wiz-net.jp>
2021-02-22 17:13:11 +0900

    課題提出

--- Calucurator.java
+++ Calucurator.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Calucurator {
+//クラス名: Calucurator
+//        メソッド： 足し算
+//        引数: a, b
+//        戻り値: int
+
+//        メソッド： 引き算
+//        引数: a, b
+//        戻り値: int
+
+//        メソッド： かけ算
+//        引数: a, b
+//        戻り値: int
+
+//        メソッド： わり算
+//        引数: a, b
+//        戻り値: int
+
+public class Calucurator {
+
+    public static int plus(int a, int b) {
+        int plus = a+b;
+        return  plus;
+    }
+
+    public static int minus(int a, int b) {
+        int minus = a-b;
+        return  minus;
+    }
+
+    public static int mul(int a, int b) {
+        int mul = a*b;
+        return  mul;
+    }
+
+    public static int divi(int a, int b) {
+        int divi = a/b;
+        return divi;
+    }
 }
