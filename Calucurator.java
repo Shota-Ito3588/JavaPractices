045e7dd53b8e92bda8b1243a21cd282a2a6a6c13
Shota <s.ito@wiz-net.jp>
2021-03-01 14:46:44 +0900

    Merge remote-tracking branch 'origin/main' into main

--- Calucurator.java
+++ Calucurator.java
@@ -17,48 +17,43 @@
 
 public class Calucurator {
 
-    public static int main(String[] args){
+    public static void main(String[] args){
 
         int a = 1;
         int b = 2;
 
-    public static int plus() {
+    public static int plus(){
 
         int ans = a+b;
 
         return ans;
     }
-//    System.out.println(int plus);
-
+//
 //    public static int minus(int a, int b) {
 //        int minus = a-b;
 //        return  minus;
 //    }
-////    System.out.println(int minus);
 //
 //    public static int mul(int a, int b) {
 //        int mul = a*b;
 //        return  mul;
 //    }
-////    System.out.println(int mul);
 //
 //    public static int divi(int a, int b) {
 //        int divi = a/b;
 //        return divi;
-    }
-
-//    System.out.println(int divi);
-
+//    }
+//
 //    void show(){
 //        System.out.println(int minus);
-    }
+//    }
 //}
-
+//
 //class JavaSample
 //{
 //    public static void main(String[] args)
 //    {
 //
-//    }
-//}
+   }
+}
 
