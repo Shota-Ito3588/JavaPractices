a0565fd9899e69a543dd4dc0a8f2d75f462972ed
Shota <s.ito@wiz-net.jp>
2021-03-02 14:48:56 +0900

    提出

--- Calucurator.java
+++ Calucurator.java
@@ -42,12 +42,22 @@ public class Calucurator {
         int ans2 = Calucurator.mul(5, 2);
         int ans3 = Calucurator.plus(4, 4);
         int ans4 = Calucurator.minus(4, 4);
+        int ans5 = Calucurator.divi(10,5);
+        int ans6 = Calucurator.plusMinus(10, 5, 6);
+        int ans7 = Calucurator.plusMul(2, 3, 6);
+        int ans8 = Calucurator.minusMul(3, 2, 5);
+        int ans9 = Calucurator.plusMinusMul(2, 5, 3, 2);
 
         System.out.println(ans);
         System.out.println(ans1);
         System.out.println(ans2);
         System.out.println(ans3);
         System.out.println(ans4);
+        System.out.println(ans5);
+        System.out.println(ans6);
+        System.out.println(ans7);
+        System.out.println(ans8);
+        System.out.println(ans9);
 
     }
 
@@ -70,22 +80,24 @@ public class Calucurator {
         int divi = a / b;
         return divi;
     }
+
+    public static int plusMinus(int target1, int target2, int target3) {
+        int plusMinus = target1 + target2 - target3;
+        return plusMinus;
+    }
+
+    public static int plusMul(int target1, int target2, int target3) {
+        int plusMul = (target1 + target2) * target3;
+        return plusMul;
+    }
+
+    public static int minusMul(int target1, int target2, int target3) {
+        int minusMul = (target1 - target2) * target3;
+        return minusMul;
+    }
+
+    public static int plusMinusMul(int target1, int target2, int target3, int target4) {
+        int plusMinusMul = ((target1 + target2) - target3) * target4 ;
+        return plusMinusMul;
+    }
 }
-//    public static void show(){
-//
-//    }
-
-//
-//    void show(){
-//        System.out.println(int minus);
-//    }
-//}
-//
-//class JavaSample
-//{
-//    public static void main(String[] args)
-//    {
-//
-//   }
-//}
-
