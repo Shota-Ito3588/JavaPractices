7961d6d9b17fd2346b801865ccaba387dcc0c9a2
sameshima <jawssame7@gmail.com>
2021-03-05 17:01:07 +0900

    文字列の問題 実行クラス 追加

---
+++ StringOperationMain.java
@@ -0,0 +1,9 @@
+public class StringOperationMain {
+
+    public static void main(String[] args) {
+
+        StringOperation ope = new StringOperation();
+        String front = ope.getPostalCodeFront("899-0999");
+
+    }
+}
